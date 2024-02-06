83c97bf7a920f901c12e38411461779cfd64da2d
gratteciel <emilian.mitu@edu.ece.fr>
2024-02-06 16:00:41 +0100

    feat: exec

--- Logiciel/obj/Release/net8.0-windows/Logiciel.AssemblyInfo.cs
+++ Logiciel/obj/Release/net8.0-windows/Logiciel.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logiciel")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+260dbbfe17dc12a166bb5f1b7344cb76fd05f3a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logiciel")]
